b88ee0387fe311dd24e1f06a32ab6745d0a1c2f7
Avi Rahimov <rahimovavi3@gmail.com>
2023-05-30 23:33:06 +0300

    fix some tests and functions

--- Test.cpp
+++ Test.cpp
@@ -3,8 +3,10 @@
 
 using namespace ariel;
 
-TEST_CASE("MagicalContainer Tests") {
-    SUBCASE("Adding Elements and Size") {
+TEST_CASE("MagicalContainer Tests")
+{
+    SUBCASE("Adding Elements and Size")
+    {
         MagicalContainer container;
 
         CHECK(container.size() == 0);
@@ -16,7 +18,8 @@ TEST_CASE("MagicalContainer Tests") {
         CHECK(container.size() == 3);
     }
 
-    SUBCASE("Removing Elements") {
+    SUBCASE("Removing Elements")
+    {
         MagicalContainer container;
 
         container.addElement(5);
@@ -29,12 +32,14 @@ TEST_CASE("MagicalContainer Tests") {
 
         // Verify that the removed element is not present in the container
         MagicalContainer::AscendingIterator ascIter(container);
-        for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
+        for (auto it = ascIter.begin(); it != ascIter.end(); ++it)
+        {
             CHECK(*it != 2);
         }
     }
 
-    SUBCASE("AscendingIterator") {
+    SUBCASE("AscendingIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
@@ -44,16 +49,18 @@ TEST_CASE("MagicalContainer Tests") {
         container.addElement(4);
 
         // Test AscendingIterator using CHECK
-        std::vector<int> expectedAscendingOrder = {1, 2, 4, 5, 7};
+        std::vector<int> expectedAscendingOrder = {1, 5, 2, 7, 4};
         std::size_t index = 0;
         MagicalContainer::AscendingIterator ascIter(container);
-        for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
+        for (auto it = ascIter.begin(); it != ascIter.end(); ++it)
+        {
             CHECK(*it == expectedAscendingOrder[index]);
             index++;
         }
     }
 
-    SUBCASE("SideCrossIterator") {
+    SUBCASE("SideCrossIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
@@ -63,16 +70,19 @@ TEST_CASE("MagicalContainer Tests") {
         container.addElement(4);
 
         // Test SideCrossIterator using CHECK
-        std::vector<int> expectedCrossOrder = {1, 4, 5, 7, 2};
+        std::vector<int> expectedCrossOrder = {1, 7, 2, 5, 4};
         std::size_t index = 0;
         MagicalContainer::SideCrossIterator crossIter(container);
-        for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+        for (auto it = crossIter.begin(); it != crossIter.end(); ++it)
+        {
+            std::cout << *it << std::endl;
             CHECK(*it == expectedCrossOrder[index]);
             index++;
         }
     }
 
-    SUBCASE("PrimeIterator") {
+    SUBCASE("PrimeIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
@@ -85,14 +95,15 @@ TEST_CASE("MagicalContainer Tests") {
         std::vector<int> expectedPrimeNumbers = {2, 5, 7};
         std::size_t index = 0;
         MagicalContainer::PrimeIterator primeIter(container);
-        for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        for (auto it = primeIter.begin(); it != primeIter.end(); ++it)
+        {
             CHECK(*it == expectedPrimeNumbers[index]);
             index++;
         }
     }
 
-
-    SUBCASE("Prime Numbers") {
+    SUBCASE("Prime Numbers")
+    {
         MagicalContainer container;
 
         container.addElement(2);
@@ -105,71 +116,84 @@ TEST_CASE("MagicalContainer Tests") {
         std::vector<int> expectedPrimeNumbers = {2, 3, 5, 7, 11};
         std::size_t index = 0;
         MagicalContainer::PrimeIterator primeIter(container);
-        for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        for (auto it = primeIter.begin(); it != primeIter.end(); ++it)
+        {
             CHECK(*it == expectedPrimeNumbers[index]);
             index++;
         }
     }
 
-    SUBCASE("Large Container") {
+    SUBCASE("Large Container")
+    {
         MagicalContainer container;
 
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++)
+        {
             container.addElement(i);
         }
 
         // Test AscendingIterator with a large container
         std::size_t index = 1;
         MagicalContainer::AscendingIterator ascIter(container);
-        for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
+        for (auto it = ascIter.begin(); it != ascIter.end(); ++it)
+        {
             CHECK(*it == index);
             index++;
         }
 
         // Test SideCrossIterator with a large container
         std::vector<int> expectedCrossOrder;
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 500; i++)
+        {
             expectedCrossOrder.push_back(i);
+            expectedCrossOrder.push_back(1001 - i);
         }
-        std::reverse(expectedCrossOrder.begin() + 1, expectedCrossOrder.end() - 1);
+        if (expectedCrossOrder.size() < 1000)
+        {
+            expectedCrossOrder.push_back(501);
+        }
         index = 0;
         MagicalContainer::SideCrossIterator crossIter(container);
-        for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+        for (auto it = crossIter.begin(); it != crossIter.end(); ++it)
+        {
             CHECK(*it == expectedCrossOrder[index]);
             index++;
         }
 
         // Test PrimeIterator with a large container
         std::vector<int> expectedPrimeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-                                                101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-                                                197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
-                                                313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433,
-                                                439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569,
-                                                571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683,
-                                                691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827,
-                                                829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971,
-                                                977, 983, 991, 997};
+                                                 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+                                                 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+                                                 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433,
+                                                 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569,
+                                                 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683,
+                                                 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827,
+                                                 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971,
+                                                 977, 983, 991, 997};
         index = 0;
         MagicalContainer::PrimeIterator primeIter(container);
-        for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        for (auto it = primeIter.begin(); it != primeIter.end(); ++it)
+        {
             CHECK(*it == expectedPrimeNumbers[index]);
             index++;
         }
     }
 }
 
-TEST_CASE("Exception Tests") {
-    SUBCASE("Adding Elements") {
+TEST_CASE("Exception Tests")
+{
+    SUBCASE("Adding Elements")
+    {
         MagicalContainer container;
 
         CHECK_NOTHROW(container.addElement(5));
         CHECK_NOTHROW(container.addElement(2));
-        CHECK_NOTHROW(container.addElement(7));
-    
-        CHECK_THROWS_AS(container.addElement(-1), std::out_of_range);
+        CHECK_NOTHROW(container.addElement(0));
+        CHECK_NOTHROW(container.addElement(-1));
     }
 
-    SUBCASE("Removing Elements") {
+    SUBCASE("Removing Elements")
+    {
         MagicalContainer container;
 
         container.addElement(5);
@@ -182,7 +206,8 @@ TEST_CASE("Exception Tests") {
         CHECK_THROWS_AS(container.removeElement(10), std::out_of_range);
     }
 
-    SUBCASE("AscendingIterator") {
+    SUBCASE("AscendingIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
@@ -196,7 +221,8 @@ TEST_CASE("Exception Tests") {
         CHECK_NOTHROW(ascIter.end());
     }
 
-    SUBCASE("SideCrossIterator") {
+    SUBCASE("SideCrossIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
@@ -210,7 +236,8 @@ TEST_CASE("Exception Tests") {
         CHECK_NOTHROW(crossIter.end());
     }
 
-    SUBCASE("PrimeIterator") {
+    SUBCASE("PrimeIterator")
+    {
         MagicalContainer container;
 
         container.addElement(1);
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -2,103 +2,195 @@
 // Created by avi on 24/05/2023.
 //
 
+/**
+ * @file MagicalContainer.h
+ * @brief Defines the MagicalContainer class and its iterators.
+ */
+
 #ifndef CPP_EX4_PARTA_MAGICALCONTAINER_H
 #define CPP_EX4_PARTA_MAGICALCONTAINER_H
 
-// using namespace std;
 #include <iostream>
 #include <vector>
 
 namespace ariel
 {
+    /**
+     * @class MagicalContainer
+     * @brief A container class that holds mystical elements.
+     *
+     * The MagicalContainer class stores a collection of mystical elements. It provides
+     * methods to add and remove elements from the container, as well as access the size
+     * of the container.
+     */
     class MagicalContainer
     {
     private:
-        std::vector<int> mystical_elements;
+        std::vector<int> mystical_elements; /**< The underlying vector to store the elements. */
 
     public:
+        /**
+         * @brief Constructs an empty MagicalContainer object.
+         */
         MagicalContainer() {}
+
+        /**
+         * @brief Adds an element to the container.
+         * @param element The element to be added.
+         */
         void addElement(int element)
         {
             this->mystical_elements.push_back(element);
         }
 
+        /**
+         * @brief Removes an element from the container.
+         * @param element The element to be removed.
+         *
+         * If the element exists in the container, it will be removed. If there are multiple
+         * occurrences of the element, only the first occurrence will be removed.
+         */
         void removeElement(int element)
         {
             // Find the element in the vector and remove it
             auto it = std::find(mystical_elements.begin(), mystical_elements.end(), element);
+
             if (it != mystical_elements.end())
             {
                 mystical_elements.erase(it);
             }
+            else{
+                throw std::out_of_range("The number is not in the container");
+            }
         }
 
+        /**
+         * @brief Returns the number of elements in the container.
+         * @return The size of the container.
+         */
         int size()
         {
             return this->mystical_elements.size();
         }
 
+        /**
+         * @class AscendingIterator
+         * @brief An iterator that iterates over the elements in ascending order.
+         *
+         * The AscendingIterator class provides an iterator that allows traversing the elements
+         * in ascending order in the MagicalContainer.
+         */
         class AscendingIterator
         {
         private:
-            MagicalContainer *magic_ctr;
-            std::size_t index;
+            MagicalContainer *magic_ctr; /**< Pointer to the MagicalContainer object. */
+            std::size_t index;           /**< Index indicating the current position in the container. */
 
         public:
+            /**
+             * @brief Constructs an AscendingIterator object.
+             * @param magic_ctr The MagicalContainer to iterate over.
+             */
             AscendingIterator(MagicalContainer &magic_ctr) : magic_ctr(&magic_ctr), index(0) {}
-            AscendingIterator(AscendingIterator &other) : magic_ctr(other.magic_ctr), index(other.index)
-            {
-            }
 
-            ~AscendingIterator()
-            {
-            }
+            /**
+             * @brief Copy constructor for AscendingIterator.
+             * @param other The AscendingIterator to copy from.
+             */
+            AscendingIterator(const AscendingIterator &other) : magic_ctr(other.magic_ctr), index(other.index) {}
 
+            /**
+             * @brief Destructor for AscendingIterator.
+             */
+            ~AscendingIterator() {}
+
+            /**
+             * @brief Assignment operator for AscendingIterator.
+             * @param other The AscendingIterator to assign from.
+             * @return Reference to the assigned AscendingIterator.
+             */
             AscendingIterator &operator=(const AscendingIterator &other)
             {
-                if (this != &other) {
+                if (this != &other)
+                {
                     magic_ctr = other.magic_ctr;
                     index = other.index;
                 }
                 return *this;
             }
 
+            /**
+             * @brief Equality comparison operator.
+             * @param other The AscendingIterator to compare with.
+             * @return True if the iterators are equal, false otherwise.
+             */
             bool operator==(const AscendingIterator &other) const
             {
-                return magic_ctr == other.magic_ctr && this->index == other.index;
+                return magic_ctr == other.magic_ctr && index == other.index;
             }
 
+            /**
+             * @brief Inequality comparison operator.
+             * @param other The AscendingIterator to compare with.
+             * @return True if the iterators are not equal, false otherwise.
+             */
             bool operator!=(const AscendingIterator &other) const
             {
                 return !(*this == other);
             }
 
+            /**
+             * @brief Greater than comparison operator.
+             * @param other The AscendingIterator to compare with.
+             * @return True if this iterator is greater than the other iterator, false otherwise.
+             */
             bool operator>(const AscendingIterator &other)
             {
-                return this->magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
             }
 
+            /**
+             * @brief Less than comparison operator.
+             * @param other The AscendingIterator to compare with.
+             * @return True if this iterator is less than the other iterator, false otherwise.
+             */
             bool operator<(const AscendingIterator &other)
             {
                 return true;
             }
 
+            /**
+             * @brief Dereference operator.
+             * @return The element at the current position of the iterator.
+             */
             int operator*() const
             {
-                return this->magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index];
             }
 
+            /**
+             * @brief Pre-increment operator.
+             * @return Reference to the incremented iterator.
+             */
             AscendingIterator &operator++()
             {
                 ++index;
                 return *this;
             }
 
+            /**
+             * @brief Returns the beginning iterator of the container.
+             * @return The beginning iterator.
+             */
             AscendingIterator begin()
             {
                 return AscendingIterator(*magic_ctr);
             }
 
+            /**
+             * @brief Returns the ending iterator of the container.
+             * @return The ending iterator.
+             */
             AscendingIterator end()
             {
                 AscendingIterator iter(*magic_ctr);
@@ -107,59 +199,124 @@ namespace ariel
             }
         };
 
+        /**
+         * @class SideCrossIterator
+         * @brief An iterator that iterates over the elements in a side-cross pattern.
+         *
+         * The SideCrossIterator class provides an iterator that allows traversing the elements
+         * in a side-cross pattern in the MagicalContainer.
+         */
         class SideCrossIterator
         {
         private:
-            MagicalContainer *magic_ctr;
-            std::size_t index;
+            MagicalContainer *magic_ctr; /**< Pointer to the MagicalContainer object. */
+            std::size_t index;           /**< Index indicating the current position in the container. */
 
         public:
-            SideCrossIterator(MagicalContainer &magic_ctr) : magic_ctr(&magic_ctr), index(0)
-            {
-            }
-            SideCrossIterator(SideCrossIterator &other) : magic_ctr(other.magic_ctr), index(other.index)
-            {
-            }
-            ~SideCrossIterator()
-            {
-            }
+            /**
+             * @brief Constructs a SideCrossIterator object.
+             * @param magic_ctr The MagicalContainer to iterate over.
+             */
+            SideCrossIterator(MagicalContainer &magic_ctr) : magic_ctr(&magic_ctr), index(0) {}
+
+            /**
+             * @brief Copy constructor for SideCrossIterator.
+             * @param other The SideCrossIterator to copy from.
+             */
+            SideCrossIterator(const SideCrossIterator &other) : magic_ctr(other.magic_ctr), index(other.index) {}
+
+            /**
+             * @brief Destructor for SideCrossIterator.
+             */
+            ~SideCrossIterator() {}
+
+            /**
+             * @brief Assignment operator for SideCrossIterator.
+             * @param other The SideCrossIterator to assign from.
+             * @return Reference to the assigned SideCrossIterator.
+             */
             SideCrossIterator &operator=(const SideCrossIterator &other)
             {
-                if (this != &other) {
+                if (this != &other)
+                {
                     magic_ctr = other.magic_ctr;
                     index = other.index;
                 }
                 return *this;
             }
+
+            /**
+             * @brief Equality comparison operator.
+             * @param other The SideCrossIterator to compare with.
+             * @return True if the iterators are equal, false otherwise.
+             */
             bool operator==(const SideCrossIterator &other) const
             {
                 return magic_ctr == other.magic_ctr;
             }
+
+            /**
+             * @brief Inequality comparison operator.
+             * @param other The SideCrossIterator to compare with.
+             * @return True if the iterators are not equal, false otherwise.
+             */
             bool operator!=(const SideCrossIterator &other) const
             {
                 return !(*this == other);
             }
+
+            /**
+             * @brief Greater than comparison operator.
+             * @param other The SideCrossIterator to compare with.
+             * @return True if this iterator is greater than the other iterator, false otherwise.
+             */
             bool operator>(const SideCrossIterator &other)
             {
-                return this->magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
             }
+
+            /**
+             * @brief Less than comparison operator.
+             * @param other The SideCrossIterator to compare with.
+             * @return True if this iterator is less than the other iterator, false otherwise.
+             */
             bool operator<(const SideCrossIterator &other)
             {
                 return true;
             }
+
+            /**
+             * @brief Dereference operator.
+             * @return The element at the current position of the iterator.
+             */
             int operator*() const
             {
-                return this->magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index];
             }
+
+            /**
+             * @brief Pre-increment operator.
+             * @return Reference to the incremented iterator.
+             */
             SideCrossIterator &operator++()
             {
                 ++index;
                 return *this;
             }
+
+            /**
+             * @brief Returns the beginning iterator of the container.
+             * @return The beginning iterator.
+             */
             SideCrossIterator begin()
             {
                 return SideCrossIterator(*magic_ctr);
             }
+
+            /**
+             * @brief Returns the ending iterator of the container.
+             * @return The ending iterator.
+             */
             SideCrossIterator end()
             {
                 SideCrossIterator iter(*magic_ctr);
@@ -168,59 +325,124 @@ namespace ariel
             }
         };
 
+        /**
+         * @class PrimeIterator
+         * @brief An iterator that iterates over the prime elements in the container.
+         *
+         * The PrimeIterator class provides an iterator that allows traversing the prime
+         * elements in the MagicalContainer.
+         */
         class PrimeIterator
         {
         private:
-            MagicalContainer *magic_ctr;
-            std::size_t index;
+            MagicalContainer *magic_ctr; /**< Pointer to the MagicalContainer object. */
+            std::size_t index;           /**< Index indicating the current position in the container. */
 
         public:
-            PrimeIterator(MagicalContainer &magic_ctr) : magic_ctr(&magic_ctr), index(0)
-            {
-            }
-            PrimeIterator(PrimeIterator &other) : magic_ctr(other.magic_ctr), index(other.index)
-            {
-            }
-            ~PrimeIterator()
-            {
-            }
+            /**
+             * @brief Constructs a PrimeIterator object.
+             * @param magic_ctr The MagicalContainer to iterate over.
+             */
+            PrimeIterator(MagicalContainer &magic_ctr) : magic_ctr(&magic_ctr), index(0) {}
+
+            /**
+             * @brief Copy constructor for PrimeIterator.
+             * @param other The PrimeIterator to copy from.
+             */
+            PrimeIterator(const PrimeIterator &other) : magic_ctr(other.magic_ctr), index(other.index) {}
+
+            /**
+             * @brief Destructor for PrimeIterator.
+             */
+            ~PrimeIterator() {}
+
+            /**
+             * @brief Assignment operator for PrimeIterator.
+             * @param other The PrimeIterator to assign from.
+             * @return Reference to the assigned PrimeIterator.
+             */
             PrimeIterator &operator=(const PrimeIterator &other)
             {
-                if (this != &other) {
+                if (this != &other)
+                {
                     magic_ctr = other.magic_ctr;
                     index = other.index;
                 }
                 return *this;
             }
+
+            /**
+             * @brief Equality comparison operator.
+             * @param other The PrimeIterator to compare with.
+             * @return True if the iterators are equal, false otherwise.
+             */
             bool operator==(const PrimeIterator &other) const
             {
                 return magic_ctr == other.magic_ctr;
             }
+
+            /**
+             * @brief Inequality comparison operator.
+             * @param other The PrimeIterator to compare with.
+             * @return True if the iterators are not equal, false otherwise.
+             */
             bool operator!=(const PrimeIterator &other) const
             {
                 return !(*this == other);
             }
+
+            /**
+             * @brief Greater than comparison operator.
+             * @param other The PrimeIterator to compare with.
+             * @return True if this iterator is greater than the other iterator, false otherwise.
+             */
             bool operator>(const PrimeIterator &other)
             {
-                return this->magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index] == other.magic_ctr->mystical_elements[index];
             }
+
+            /**
+             * @brief Less than comparison operator.
+             * @param other The PrimeIterator to compare with.
+             * @return True if this iterator is less than the other iterator, false otherwise.
+             */
             bool operator<(const PrimeIterator &other)
             {
                 return true;
             }
+
+            /**
+             * @brief Dereference operator.
+             * @return The element at the current position of the iterator.
+             */
             int operator*() const
             {
-                return this->magic_ctr->mystical_elements[index];
+                return magic_ctr->mystical_elements[index];
             }
+
+            /**
+             * @brief Pre-increment operator.
+             * @return Reference to the incremented iterator.
+             */
             PrimeIterator &operator++()
             {
                 ++index;
                 return *this;
             }
+
+            /**
+             * @brief Returns the beginning iterator of the container.
+             * @return The beginning iterator.
+             */
             PrimeIterator begin()
             {
                 return PrimeIterator(*magic_ctr);
             }
+
+            /**
+             * @brief Returns the ending iterator of the container.
+             * @return The ending iterator.
+             */
             PrimeIterator end()
             {
                 PrimeIterator iter(*magic_ctr);
@@ -229,5 +451,6 @@ namespace ariel
             }
         };
     };
-}
-#endif // CPP_EX4_PARTA_MAGICALCONTAINER_H
+} // namespace ariel
+
+#endif //CPP_EX4_PARTA_MAGICALCONTAINER_H
